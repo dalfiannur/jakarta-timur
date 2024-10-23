a9c142344e6399639af04364f12fc0348dcc3579
dalfiannur <dalfiannur@gmail.com>
2024-10-23 12:10:21 +0700

    fix: publication breadcrumbs

--- src/app/(main)/publikasi/contants.ts
+++ src/app/(main)/publikasi/contants.ts
@@ -1,8 +1,8 @@
 export const SELECTABLE_BREADCRUMBS: {
   [key: string]: string;
 } = {
-  "berita-pemerintah-terkini": "Berita Pemerintah Terkini",
-  "video-informasi-edukasi": "Video Informasi dan Edukasi",
-  "cerita-melalui-citra": "Cerita Melalui Citra",
-  "buletin-info-jaktim": "Buletin Info Jaktim",
+  berita: "Berita Pemerintah Terkini",
+  video: "Video Informasi dan Edukasi",
+  galeri: "Cerita Melalui Citra",
+  buletin: "Buletin Info Jaktim",
 };
