a7e19935bc5b281f6b096ab56bff3ccd682394ed
dalfiannur <dalfiannur@gmail.com>
2024-11-27 11:18:40 +0700

    style: Responsive Bulletin Detail Page

--- src/app/(main)/publikasi/buletin/[id]/page.tsx
+++ src/app/(main)/publikasi/buletin/[id]/page.tsx
@@ -11,15 +11,18 @@ export default async function Page(props: PageProps) {
   const { data } = await findBulletinById(params.id);
 
   return (
-    <div>
-      <div className="mt-12 flex gap-16">
+    <div className="px-4">
+      <div className="mt-12 flex flex-col gap-16 lg:flex-row">
         <div className="flex-1">
-          <h2 className="font-bold text-4xl font-plus-jakarta-sans">
+          <h2 className="font-plus-jakarta-sans text-xl font-bold lg:text-3xl">
             {data.title}
           </h2>
 
-          <div className="mt-12 flex justify-center">
-            <iframe src={data.frame} width={1024} height={900}></iframe>
+          <div className="mt-6 flex justify-center lg:mt-12">
+            <iframe
+              src={data.frame}
+              className="aspect-[257/225] w-full lg:w-[1024px]"
+            ></iframe>
           </div>
         </div>
       </div>
